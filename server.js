d81e02419a304ebf82f35bc1d4baa38fa82eb249
mlowth4r <mlowth4r@gmail.com>
2026-09-22 12:32:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const { createClient } = require('@supabase/supabase-js');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type'],
+  credentials: false
+}));
 app.use(express.json());
 
 // Supabase config
